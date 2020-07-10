e33fcfaa28d732a3b7567c2cd616cafbf5bea1b5
karthik <karthik.talam@eHealthCorp.net>
2020-07-10 16:22:07 +0530

    retreiving battle locations has been made unique

--- Controllers/Index.controller.js
+++ Controllers/Index.controller.js
@@ -2,48 +2,44 @@ const queryString = require('query-string');
 const BattleModel = require('../models/battles');
 
 exports.battleList = async (req, res, next) => {
-    try {
-        const battles = await BattleModel.find().select('location -_id');
-        const locations = battles.map((battle) => battle.location);
-        res.send(locations);
-    } catch (error) {
-        console.error(error);
-    }
+	try {
+		const battles = await BattleModel.find().distinct('location');
+		res.send(battles);
+	} catch (error) {
+		console.error(error);
+	}
 };
 
 exports.battleCount = async (req, res, next) => {
-    try {
-        const battleCount = await BattleModel.countDocuments();
-        res.send(battleCount);
-    } catch (error) {
-        console.error(error);
-    }
+	try {
+		const battleCount = await BattleModel.countDocuments();
+		res.send(battleCount);
+	} catch (error) {
+		console.error(error);
+	}
 };
 
 exports.battleSearch = async (req, res, next) => {
-    try {
-        const {query: queries} = queryString.parseUrl(req.url);
-        const {location, type, ...rest} = queries;
-        let orCondition;
-        if (queries.king) {
-            orCondition = [
-                {attacker_king: queries.king},
-                {defender_king: queries.king},
-            ];
-        }
-        const conditions = {
-            $or: orCondition,
-            location: location,
-            battle_type: type,
-        };
-        Object.keys(conditions).forEach((key) => {
-            if (!conditions[key]) {
-                delete conditions[key];
-            }
-        });
-        const battles = await BattleModel.find(conditions);
-        res.send(battles);
-    } catch (error) {
-        console.error(error);
-    }
+	try {
+		const {query: queries} = queryString.parseUrl(req.url);
+		const {location, type, ...rest} = queries;
+		let orCondition;
+		if (queries.king) {
+			orCondition = [{attacker_king: queries.king}, {defender_king: queries.king}];
+		}
+		const conditions = {
+			$or: orCondition,
+			location: location,
+			battle_type: type,
+		};
+		Object.keys(conditions).forEach((key) => {
+			if (!conditions[key]) {
+				delete conditions[key];
+			}
+		});
+		const battles = await BattleModel.find(conditions);
+		res.send(battles);
+	} catch (error) {
+		console.error(error);
+	}
 };
